ea710c7433a35e49ec253b91c0c8217cd180a789
Baglan Sagidulla <kxelly@mail.ru>
2023-03-26 17:18:00 +0200

    initial commit

---
+++ src/main/java/com/pja/bloodcount/BloodCountApplication.java
@@ -0,0 +1,13 @@
+package com.pja.bloodcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodCountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodCountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pja/bloodcount/BloodcountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pja.bloodcount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodcountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
