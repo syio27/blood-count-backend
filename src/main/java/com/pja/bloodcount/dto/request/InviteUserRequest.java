1946a1cdd28229732e9618f0ffdb84d07b0c6dde
syio27 <baglansagidulladev@gmail.com>
2023-06-03 01:59:49 +0200

    invite api change and mail message change

--- src/main/java/com/pja/bloodcount/dto/request/InviteUserRequest.java
+++ src/main/java/com/pja/bloodcount/dto/request/InviteUserRequest.java
@@ -14,4 +14,5 @@ public class InviteUserRequest {
 
     private String email;
     private Role role;
+    private String groupNumber;
 }
--- src/main/java/com/pja/bloodcount/service/AdminService.java
+++ src/main/java/com/pja/bloodcount/service/AdminService.java
@@ -3,10 +3,14 @@ package com.pja.bloodcount.service;
 import com.pja.bloodcount.dto.request.InviteUserRequest;
 import com.pja.bloodcount.exceptions.EmailValidationException;
 import com.pja.bloodcount.exceptions.UserConflictException;
+import com.pja.bloodcount.model.Group;
 import com.pja.bloodcount.model.User;
+import com.pja.bloodcount.repository.GroupRepository;
 import com.pja.bloodcount.repository.UserRepository;
+import com.pja.bloodcount.service.contract.GroupService;
 import com.pja.bloodcount.utils.PasswordGeneratorUtil;
 import com.pja.bloodcount.utils.ValidationUtil;
+import com.pja.bloodcount.validation.GroupValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,9 +22,10 @@ import org.springframework.stereotype.Service;
 public class AdminService {
 
     private final UserRepository userRepository;
+    private final GroupRepository groupRepository;
+    private final GroupValidator groupValidator;
     private final PasswordEncoder passwordEncoder;
     private final MailService mailService;
-
 
     public void invite(InviteUserRequest inviteRequest) {
         if (!ValidationUtil.validateEmail(inviteRequest.getEmail())) {
@@ -31,24 +36,30 @@ public class AdminService {
             throw new UserConflictException(inviteRequest.getEmail());
         }
 
+        Group group = groupValidator.validateIfExistsAndGet(inviteRequest.getGroupNumber());
+
         String generatedPassword = PasswordGeneratorUtil.generateRandomPassword();
 
         log.info("User's password generated {}", generatedPassword);
 
         User user = User.builder()
-                .name("Baglan")
+                .name("Baglan") // TODO: delete name attribute for user entity
                 .email(inviteRequest.getEmail())
                 .password(passwordEncoder.encode(generatedPassword))
                 .role(inviteRequest.getRole())
                 .build();
 
+        group.addUser(user);
+
         final String profileUrl = "http://localhost:4200/profile";
         final String subject = "Invitation letter";
         final String message = "Dear " + user.getName() + ",\n\n"
-                + "You have been invited to join our application as a supervisor.\n\n"
+                + "You have been invited to join our application as a " + inviteRequest.getRole().toString().toLowerCase() + ".\n\n"
                 + "Here are your login credentials:\n"
                 + "Email: " + inviteRequest.getEmail() + "\n"
-                + "Temporary password: " + generatedPassword + "\n\n"
+                + "Temporary password: " + generatedPassword + "\n"
+                + "Assigned Group: " + group.getGroupNumber() + "\n"
+                + "Group Type: " + group.getGroupType() + "\n\n"
                 + "Profile page URL: " + profileUrl + "\n\n"
                 + "Please log in using these details. We strongly recommend that you change your password immediately after your first login for security purposes.\n\n"
                 + "If you have any issues, please feel free to contact us.\n\n"
