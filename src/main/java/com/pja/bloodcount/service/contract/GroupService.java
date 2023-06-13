f355ab4795386bc83ff9864c90f032e8978b5826
syio27 <baglansagidulladev@gmail.com>
2023-06-14 00:37:10 +0200

    delete user from group api

--- src/main/java/com/pja/bloodcount/service/contract/GroupService.java
+++ src/main/java/com/pja/bloodcount/service/contract/GroupService.java
@@ -5,6 +5,7 @@ import com.pja.bloodcount.dto.response.GroupResponse;
 import com.pja.bloodcount.model.enums.GroupType;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface GroupService {
     GroupResponse getGroupByNumber(String groupNumber);
@@ -12,4 +13,6 @@ public interface GroupService {
     List<GroupResponse> getAllGroups();
     List<GroupResponse> getAllGroupsByType(GroupType type);
     void clearGroupFromUsers(String groupNumber);
+    void deleteGroup(String groupNumber);
+    void deleteUserFromGroup(String groupNumber, UUID userId);
 }
