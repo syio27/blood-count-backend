5f12029782c87452777179ee1cccdf4831dbd012
syio27 <baglansagidulladev@gmail.com>
2023-06-21 23:46:10 +0200

    fix

--- src/main/java/com/pja/bloodcount/BloodCountApplication.java
+++ src/main/java/com/pja/bloodcount/BloodCountApplication.java
@@ -2,10 +2,12 @@ package com.pja.bloodcount;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableConfigurationProperties
 public class BloodCountApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/pja/bloodcount/config/PropertiesConfig.java
+++ src/main/java/com/pja/bloodcount/config/PropertiesConfig.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 import javax.validation.constraints.NotNull;
 
 @Component
-@EnableConfigurationProperties
 @ConfigurationProperties(prefix = "app")
 @Data
 public class PropertiesConfig {
