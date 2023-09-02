b3c2b34f736d1325e433ee6d48ea4a206f864aba
syio27 <baglansagidulladev@gmail.com>
2023-09-02 02:15:36 +0200

    return back 30 mins for game

--- src/main/java/com/pja/bloodcount/service/GameService.java
+++ src/main/java/com/pja/bloodcount/service/GameService.java
@@ -49,7 +49,7 @@ public class GameService {
         Patient patient = generationService.generatePatient(caseId);
         Case aCase = caseValidator.validateIfExistsAndGet(caseId);
         generationService.generateBloodCount(caseId, patient.getId());
-        int durationInMin = 2;
+        int durationInMin = 30;
         int durationInSec = durationInMin * 60;
         Instant endTime = Instant.now().plusSeconds(durationInSec);
 
