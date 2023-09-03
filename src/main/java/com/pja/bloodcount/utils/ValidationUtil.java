43389f9e80b3d0f0a0198057ebf9e89c6497e59c
syio27 <baglansagidulladev@gmail.com>
2023-09-04 01:43:01 +0200

    domains constraint added

--- src/main/java/com/pja/bloodcount/utils/ValidationUtil.java
+++ src/main/java/com/pja/bloodcount/utils/ValidationUtil.java
@@ -10,6 +10,10 @@ import java.util.regex.Pattern;
  * @author baglan
  * @version 1.0
  */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.springframework.stereotype.Service;
+
 @Service
 public class ValidationUtil {
 
@@ -28,22 +32,24 @@ public class ValidationUtil {
 
     /**
      * Email regex pattern
-     * ^                 # start-of-string
-     * [A-Z0-9._%+-]+    # match characters and symbols in the list, a-z, 0-9, underscore, dot, percent, plus, hyphen
-     * @                 # must contain @ symbol
-     * [A-Z0-9.-]+       # match characters and symbols in the list, a-z, 0-9, dot, hyphen
-     * \.                # must contain a dot character
-     * {2,6}             # match between 2 and 6 characters
-     * $                 # end-of-string
+     * ^                         # start-of-string
+     * [A-Z0-9._%+-]+            # match characters and symbols in the list, a-z, 0-9, underscore, dot, percent, plus, hyphen
+     * @                         # must contain @ symbol
+     * (student\.wum\.edu\.pl    # must contain student.wum.edu.pl domain
+     * |wum\.edu\.pl             # OR must contain wum.edu.pl domain
+     * |gmail\.com)              # OR must contain gmail.com domain
+     * $                         # end-of-string
      */
-    private static final Pattern EMAIL_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+    private static final Pattern EMAIL_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@((student\\.wum\\.edu\\.pl)|(wum\\.edu\\.pl)|(gmail\\.com))$", Pattern.CASE_INSENSITIVE);
 
     public static boolean validatePassword(String password) {
         Matcher matcher = PASSWORD_REGEX.matcher(password);
         return matcher.matches();
     }
+
     public static boolean validateEmail(String email) {
         Matcher matcher = EMAIL_REGEX.matcher(email);
         return matcher.matches();
     }
 }
+
